947992e60c8e26dd4ab7cf448cd979c93181f357
jacobhjustice <jacobhj1997@gmail.com>
2019-11-16 17:37:55 -0600

    refactored into util class in order to help readability and seperate functions from the player/human turn model

--- index.js
+++ index.js
@@ -1,46 +1,9 @@
-var Othello = {
-    Status: {
-        UNDETERMINED: -1,
-        WHITE: 1,
-        BLACK: 0
-    },
-    
+var Othello = {  
     Cell: function(row, column, status) {
         this.row = row;
         this.column = column;
         this.status = status;
         this.selectable = false;
-    },
-
-    // Given an index and whether or not it corresponds to a row
-    // Return whether or not the index is valid
-    checkIndex(index, isRow) {
-        if (isRow) {
-            return (index < this.ROW_SIZE && index >= 0);
-        }
-        return (index < this.COL_SIZE && index >= 0)
-    },
-
-    // Given a set of cells, and a specific cell in the set
-    // Find the neighbors of cell
-    getNeighborCells(cells, cell) {
-        neighbors = [];
-        for (var r = -1; r <= 1; r++) {
-            var rowIndex = cell.row + r;
-            if (this.checkIndex(rowIndex, true)) {
-                var currentRow = cells[rowIndex];
-                for (var c = -1; c <= 1; c++) {
-                    var colIndex = cell.column + c;
-                    if (this.checkIndex(colIndex, false)) {
-                        var neighboringCell = currentRow[colIndex];
-                        if (neighboringCell != cell) {
-                            neighbors.push(neighboringCell);
-                        }
-                    }
-                }
-            }
-        }
-        return neighbors;
     },
 
     UI: {
@@ -76,8 +39,6 @@ var Othello = {
             this.board.rows = cells;
         }
     },
-    ROW_SIZE: 8,
-    COL_SIZE:  8,
 
     currentGameCells: [],
     playerTurn: -1,
@@ -85,15 +46,15 @@ var Othello = {
     initialize: function() {
         // Create the set of cells
         this.currentGameCells = [];
-        for (var r = 0; r < this.ROW_SIZE; r++) {
+        for (var r = 0; r < this.util.ROW_SIZE; r++) {
             var row = [];
-            for (var c = 0; c < this.COL_SIZE; c++) {
-                var status = this.Status.UNDETERMINED;
-                if ((r == this.ROW_SIZE / 2 && c == this.COL_SIZE / 2) || (r + 1 == this.ROW_SIZE / 2 && c + 1 == this.COL_SIZE / 2)) {
-                    status = this.Status.BLACK;
+            for (var c = 0; c < this.util.COL_SIZE; c++) {
+                var status = this.util.Status.UNDETERMINED;
+                if ((r == this.util.ROW_SIZE / 2 && c == this.util.COL_SIZE / 2) || (r + 1 == this.util.ROW_SIZE / 2 && c + 1 == this.util.COL_SIZE / 2)) {
+                    status = this.util.Status.BLACK;
                 }
-                else if ((r + 1 == this.ROW_SIZE / 2 && c == this.COL_SIZE / 2) || (r == this.ROW_SIZE / 2 && c + 1 == this.COL_SIZE / 2)) {
-                    status = this.Status.WHITE;
+                else if ((r + 1 == this.util.ROW_SIZE / 2 && c == this.util.COL_SIZE / 2) || (r == this.util.ROW_SIZE / 2 && c + 1 == this.util.COL_SIZE / 2)) {
+                    status = this.util.Status.WHITE;
                 }
                 row.push(new this.Cell(r, c, status));
             }
@@ -101,10 +62,10 @@ var Othello = {
         }
 
         // Set Black to go first
-        this.playerTurn = this.Status.BLACK;
+        this.playerTurn = this.util.Status.BLACK;
 
         // Set human player to black
-        this.humanPlayer = this.Status.BLACK;
+        this.humanPlayer = this.util.Status.BLACK;
 
         // Initialize UI
         this.UI.initializeMessageComponent();
@@ -122,27 +83,6 @@ var Othello = {
         
     }, 
 
-    // Given a set of cells
-    // Reset the selectable property in each infividual cell to false
-    resetSelectableCells: function(cells) {
-        cells.forEach(row => {
-            row.forEach(cell => {
-                cell.selectable = false;
-            })
-        });
-    }, 
-
-    // Given a set of cells
-    // Update all potential cells by the rules of Othello to have selectable attribute = true
-    updateSelectableCells: function(cells) {
-        this.resetSelectableCells(cells);
-
-        selectableCells = this.getSelectableCells(cells);
-        selectableCells.forEach(cell => {
-            cell.selectable = true;
-        });
-    },
-
     turn: function() {
         if (this.humanPlayer == this.playerTurn) {
             this.humanTurn();
@@ -151,61 +91,9 @@ var Othello = {
         }
     },
 
-    // Given a row and column to the currentGameCells
-    // Set that cell to the current player's move.
-    move: function(row, column) {
-        var cells = this.currentGameCells;
-        cell = cells[row][column]
-        cell.status = this.playerTurn;
-        this.flipEffectedCellsFromMove(cells, cell)
-        this.playerTurn = this.getOpposingPlayer(this.playerTurn);
-
-        this.turn();
-    },
-
-    // Given a set of cells and a movedCell that has just been placed down
-    // Evaluate the new grid after flipping "sandwhiched" cells by following the ruled of Othello
-    flipEffectedCellsFromMove: function(cells, movedCell) {
-        var neighbors = this.getNeighborCells(cells, movedCell);
-        var opposingColor = this.getOpposingPlayer(this.playerTurn);
-        neighbors.forEach(cell => {
-            if (cell.status == opposingColor) {
-                var directionR = cell.row - movedCell.row;
-                var directionC = cell.column - movedCell.column;
-                var failedFind = false;
-                var middleCells = [cell];
-                var current = cell;
-                // TODO extract incremental trail logic
-                do {
-                    // Increment to next spot, and validate.
-                    var rowIndex = current.row + directionR
-                    var colIndex = current.column + directionC
-                    if (!this.checkIndex(rowIndex, true) || !this.checkIndex(colIndex, false)) {
-                        failedFind = true
-                        break;
-                    }
-
-                    // Move to next cell, and if it is not of the opponent's color, we failed.
-                    current = cells[rowIndex][colIndex];
-                    if (current.status == this.Status.UNDETERMINED) {
-                        failedFind = true;
-                        break;
-                    }
-                    middleCells.push(current)
-                } while(current.status != this.playerTurn)
-                if (!failedFind) {
-                    middleCells.forEach(cell => {
-                        cell.status = this.playerTurn;
-                    });
-                }
-            }
-
-        });
-    },
-
     humanTurn: function() {
         this.UI.setTurnMessageHuman();
-        this.updateSelectableCells(this.currentGameCells);
+        this.util.updateSelectableCells(this.currentGameCells, this.playerTurn);
         this.UI.updateBoard(this.currentGameCells);
     },
 
@@ -222,69 +110,183 @@ var Othello = {
         // If needed, consider creating a new class/struct to encapsulate all AI related computations
     },
 
-    // Given a set of cells
-    // Return an array of all cells that are selectable within that set
-    getSelectableCells: function(cells) {
-        contenders = [];
-        var currentTurn = this.playerTurn;
-        var potentialAdjacentColor = this.getOpposingPlayer(currentTurn);
-        for (var r = 0; r < this.ROW_SIZE; r++) {
-            for (var c = 0; c < this.COL_SIZE; c++) {
-                var cell = cells[r][c];
+    // Given a row and column to the currentGameCells
+    // Set that cell to the current player's move.
+    move: function(row, column) {
+        var cells = this.currentGameCells;
+        cell = cells[row][column]
+        cell.status = this.playerTurn;
+        this.util.flipEffectedCellsFromMove(cells, cell, this.playerTurn)
+        this.playerTurn = this.util.getOpposingPlayer(this.playerTurn);
 
-                // Check to see if cell is avaliable
-                if (cell.status != this.Status.UNDETERMINED) {
-                    continue;
-                }
-                
-                // Check neighbors of opposing color
-                var neighbors = this.getNeighborCells(cells, cell);
-                for (var i = 0; i < neighbors.length; i++) {
-                    neighbor = neighbors[i];
-                    if (neighbor.status == potentialAdjacentColor) {
-                        // Check to see if you continue in this direction, if you hit current players color.
-                        // If so, add to contenders list.
-                        // If not, continue to next neighbor.
-                        var directionR = neighbor.row - cell.row;
-                        var directionC = neighbor.column - cell.column;
-                        var current = neighbor;
-                        var failedFind = false;
-                        do {
-                            // Increment to next spot, and validate.
-                            var rowIndex = current.row + directionR
-                            var colIndex = current.column + directionC
-                            if (!this.checkIndex(rowIndex, true) || !this.checkIndex(colIndex, false)) {
-                                failedFind = true
-                                break;
-                            }
+        this.turn();
+    },
 
-                            // Move to next cell, and if it is not of the opponent's color, we failed.
-                            current = cells[rowIndex][colIndex];
-                            if (current.status == this.Status.UNDETERMINED) {
-                                failedFind = true;
-                                break;
+    util: {
+        ROW_SIZE: 8,
+        COL_SIZE:  8,
+        
+        // Given an index and whether or not it corresponds to a row
+        // Return whether or not the index is valid
+        checkIndex(index, isRow) {
+            if (isRow) {
+                return (index < this.ROW_SIZE && index >= 0);
+            }
+            return (index < this.COL_SIZE && index >= 0)
+        },
+
+        // Given a set of cells, and a specific cell in the set
+        // Find the neighbors of cell
+        getNeighborCells(cells, cell) {
+            neighbors = [];
+            for (var r = -1; r <= 1; r++) {
+                var rowIndex = cell.row + r;
+                if (this.checkIndex(rowIndex, true)) {
+                    var currentRow = cells[rowIndex];
+                    for (var c = -1; c <= 1; c++) {
+                        var colIndex = cell.column + c;
+                        if (this.checkIndex(colIndex, false)) {
+                            var neighboringCell = currentRow[colIndex];
+                            if (neighboringCell != cell) {
+                                neighbors.push(neighboringCell);
                             }
-                        } while(current.status != currentTurn)
-                        if (!failedFind) {
-                            contenders.push(cell);
-                            break;
                         }
                     }
                 }
             }
-        }
-        return contenders;
-    },
+            return neighbors;
+        },
 
-    // Given the current player
-    // Return the opposite player
-    getOpposingPlayer: function(currentTurn) {
-        var testPlayer = this.Status.BLACK;
-        if (testPlayer != currentTurn) {
-            return testPlayer;
-        }
-        return this.Status.WHITE;
-    }
+        // Given a set of cells
+        // Reset the selectable property in each infividual cell to false
+        resetSelectableCells: function(cells) {
+            cells.forEach(row => {
+                row.forEach(cell => {
+                    cell.selectable = false;
+                })
+            });
+        }, 
+
+        // Given a set of cells and the current playerTurn
+        // Update all potential cells by the rules of Othello to have selectable attribute = true
+        updateSelectableCells: function(cells, playerTurn) {
+            this.resetSelectableCells(cells);
+
+            selectableCells = this.getSelectableCells(cells, playerTurn);
+            selectableCells.forEach(cell => {
+                cell.selectable = true;
+            });
+        },
+
+        // Given a set of cells, the current playerTurn, and a movedCell that has just been placed down
+        // Evaluate the new grid after flipping "sandwhiched" cells by following the ruled of Othello
+        flipEffectedCellsFromMove: function(cells, movedCell, playerTurn) {
+            var neighbors = this.getNeighborCells(cells, movedCell);
+            var opposingColor = this.getOpposingPlayer(playerTurn);
+            neighbors.forEach(cell => {
+                if (cell.status == opposingColor) {
+                    var directionR = cell.row - movedCell.row;
+                    var directionC = cell.column - movedCell.column;
+                    var failedFind = false;
+                    var middleCells = [cell];
+                    var current = cell;
+                    // TODO extract incremental trail logic
+                    do {
+                        // Increment to next spot, and validate.
+                        var rowIndex = current.row + directionR
+                        var colIndex = current.column + directionC
+                        if (!this.checkIndex(rowIndex, true) || !this.checkIndex(colIndex, false)) {
+                            failedFind = true
+                            break;
+                        }
+
+                        // Move to next cell, and if it is not of the opponent's color, we failed.
+                        current = cells[rowIndex][colIndex];
+                        if (current.status == this.Status.UNDETERMINED) {
+                            failedFind = true;
+                            break;
+                        }
+                        middleCells.push(current)
+                    } while(current.status != playerTurn)
+                    if (!failedFind) {
+                        middleCells.forEach(cell => {
+                            cell.status = playerTurn;
+                        });
+                    }
+                }
+
+            });
+        },
+
+        // Given a set of cells and the current playerTurn
+        // Return an array of all cells that are selectable within that set
+        getSelectableCells: function(cells, currentTurn) {
+            contenders = [];
+            var potentialAdjacentColor = this.getOpposingPlayer(currentTurn);
+            for (var r = 0; r < this.ROW_SIZE; r++) {
+                for (var c = 0; c < this.COL_SIZE; c++) {
+                    var cell = cells[r][c];
+
+                    // Check to see if cell is avaliable
+                    if (cell.status != this.Status.UNDETERMINED) {
+                        continue;
+                    }
+                    
+                    // Check neighbors of opposing color
+                    var neighbors = this.getNeighborCells(cells, cell);
+                    for (var i = 0; i < neighbors.length; i++) {
+                        neighbor = neighbors[i];
+                        if (neighbor.status == potentialAdjacentColor) {
+                            // Check to see if you continue in this direction, if you hit current players color.
+                            // If so, add to contenders list.
+                            // If not, continue to next neighbor.
+                            var directionR = neighbor.row - cell.row;
+                            var directionC = neighbor.column - cell.column;
+                            var current = neighbor;
+                            var failedFind = false;
+                            do {
+                                // Increment to next spot, and validate.
+                                var rowIndex = current.row + directionR
+                                var colIndex = current.column + directionC
+                                if (!this.checkIndex(rowIndex, true) || !this.checkIndex(colIndex, false)) {
+                                    failedFind = true
+                                    break;
+                                }
+
+                                // Move to next cell, and if it is not of the opponent's color, we failed.
+                                current = cells[rowIndex][colIndex];
+                                if (current.status == this.Status.UNDETERMINED) {
+                                    failedFind = true;
+                                    break;
+                                }
+                            } while(current.status != currentTurn)
+                            if (!failedFind) {
+                                contenders.push(cell);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            return contenders;
+        },
+
+        // Given the current player
+        // Return the opposite player
+        getOpposingPlayer: function(currentTurn) {
+            var testPlayer = this.Status.BLACK;
+            if (testPlayer != currentTurn) {
+                return testPlayer;
+            }
+            return this.Status.WHITE;
+        },
+
+        Status: {
+            UNDETERMINED: -1,
+            WHITE: 1,
+            BLACK: 0
+        },
+    },
 };
 
 Othello.initialize();
